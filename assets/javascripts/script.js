6bc3d66c96bd80d1aa93eae31696eb6cb8d8778d
congkim94 <congkim94@gmail.com>
2020-05-06 13:09:27 -0400

    initial commit

---
+++ assets/javascripts/script.js
@@ -0,0 +1,325 @@
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+    // wait until window is loaded - all images, styles-sheets, fonts, links, and other media assets
+    // you could also use addEventListener() instead
+    window.onload = function() {
+      
+       // OPTIONAL - waits til next tick render to run code (prevents running in the middle of render tick)
+       window.requestAnimationFrame(function() {
+      
+          var tl = new TimelineMax();
+
+        tl.from('.name', .7, {y:-100,autoAlpha:0, ease: Power1.easeInOut});
+        tl.from('.tag', .7, {y:100, autoAlpha:0, ease: Power1.easeInOut}, '-=.3');
+        tl.from('.mainpic', .5, {x:100,autoAlpha:0, ease: Power1.easeInOut}, '-=.8');
+        // tl.from('.shape1', .7, {x:-100, y:-150,autoAlpha:0, ease: Power1.easeInOut}, '-=.4')
+        // tl.from('.shape2', .7, {x:-100, autoAlpha:0, ease: Power1.easeInOut},'-=.5');
+
+         
+       });
+      
+    };
+  
+});
+
+
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+
+
+(function() {
+    var hamburger = {
+        navToggle: document.querySelector('.menu-toggle'),
+        nav: document.querySelector('.box'),
+        doToggle: function(e) {
+            
+            this.navToggle.classList.toggle('active');
+            this.nav.classList.toggle('active');
+        }
+    };
+    hamburger.navToggle.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+    hamburger.nav.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+}());
+
+
+
+anime({
+	targets:'.web',
+	d:[
+		{ value: 'M821.255,1307.734c33.767,123.526-138.126-15.708-138.126,79.633s36.352,234.652,96.367,135.2,93.947-39.059,150.845-39.059-83.694-79.52-8.935-156.308S821.255,1307.734,821.255,1307.734Z'},
+		{ value: 'M821.255,1307.734c-56.624,75.02-138.126-15.708-138.126,79.633s39.435,195.3,138.126,166.079-4.34-136.861,52.558-136.861,116.87-19.991,67.662-92.266S821.255,1307.734,821.255,1307.734Z'}
+	],
+	easing:'easeOutQuad',
+	duration: 4000,	
+	loop: true
+});
+
+
+anime({
+	targets:'.photo',
+	d:[
+		{ value: 'M1198.454,3221.1s16.512,62.088,73.457,17.154,79.984-93.162,112.339-17.154-66.862,57.488-36.808,100.55-202.243,129.628-148.987,26.789c-17.4-74.89-154.275-46.707-97.33-101S1198.454,3221.1,1198.454,3221.1Z'},
+		{ value: 'M1176.849,3180.007s.184,86.028,57.129,41.094,199.307-54.3,150.271,0-126.544,44.934-96.49,88-96.49,161.014-134.453,65.529c-17.4-74.89-112.308-99.23-55.363-153.525S1176.849,3180.007,1176.849,3180.007Z'}
+	],
+	easing:'easeOutQuad',
+	duration: 2000,	
+	loop: true
+});
+
+
+
+
+
+var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+    // parse slide data (url, title, size ...) from DOM elements 
+    // (children of gallerySelector)
+    var parseThumbnailElements = function(el) {
+        var thumbElements = el.childNodes,
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+            figureEl = thumbElements[i]; // <figure> element
+
+            // include only element nodes 
+            if(figureEl.nodeType !== 1) {
+                continue;
+            }
+
+            linkEl = figureEl.children[0]; // <a> element
+
+            size = linkEl.getAttribute('data-size').split('x');
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            };
+
+
+
+            if(figureEl.children.length > 1) {
+                // <figcaption> content
+                item.title = figureEl.children[1].innerHTML; 
+            }
+
+            if(linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = linkEl.children[0].getAttribute('src');
+            } 
+
+            item.el = figureEl; // save link to element for getThumbBoundsFn
+            items.push(item);
+        }
+
+        return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+    };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+        // find root element of slide
+        var clickedListItem = closest(eTarget, function(el) {
+            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        });
+
+        if(!clickedListItem) {
+            return;
+        }
+
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        var clickedGallery = clickedListItem.parentNode,
+            childNodes = clickedListItem.parentNode.childNodes,
+            numChildNodes = childNodes.length,
+            nodeIndex = 0,
+            index;
+
+        for (var i = 0; i < numChildNodes; i++) {
+            if(childNodes[i].nodeType !== 1) { 
+                continue; 
+            }
+
+            if(childNodes[i] === clickedListItem) {
+                index = nodeIndex;
+                break;
+            }
+            nodeIndex++;
+        }
+
+
+
+        if(index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe( index, clickedGallery );
+        }
+        return false;
+    };
+
+    // parse picture index and gallery index from URL (#&pid=1&gid=2)
+    var photoswipeParseHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
+
+        if(hash.length < 5) {
+            return params;
+        }
+
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');  
+            if(pair.length < 2) {
+                continue;
+            }           
+            params[pair[0]] = pair[1];
+        }
+
+        if(params.gid) {
+            params.gid = parseInt(params.gid, 10);
+        }
+
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        // define options (if needed)
+        options = {
+
+        	bgOpacity: 0.85,
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect(); 
+
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+
+        };
+
+        // PhotoSwipe opened from URL
+        if(fromURL) {
+            if(options.galleryPIDs) {
+                // parse real index when custom PIDs are used 
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for(var j = 0; j < items.length; j++) {
+                    if(items[j].pid == index) {
+                        options.index = j;
+                        break;
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1;
+            }
+        } else {
+            options.index = parseInt(index, 10);
+        }
+
+        // exit if index not found
+        if( isNaN(options.index) ) {
+            return;
+        }
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    var hashData = photoswipeParseHash();
+    if(hashData.pid && hashData.gid) {
+        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.mygallery');
+
+
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  
+  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+  // We'll also animate that scroll with requestAnimationFrame:
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    // ScrollTo takes an x and a y coordinate.
+    // Increase the '10' value to get a smoother/slower scroll!
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
+
+
