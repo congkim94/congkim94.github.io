5af9c416cfd4f218be568ab075010bafdaf10f9a
congkim94 <congkim94@gmail.com>
2020-05-22 23:58:55 -0400

    commit

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,3 +1,5 @@
+
+
 // wait until DOM is ready
 document.addEventListener("DOMContentLoaded", function(event) {
   
@@ -24,21 +26,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-// Set a variable for our button element.
-const scrollToTopButton = document.getElementById('js-top');
 
-// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
-const scrollFunc = () => {
-  // Get the current scroll value
-  let y = window.scrollY;
-  
-  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-  if (y > 0) {
-    scrollToTopButton.className = "top-link show";
-  } else {
-    scrollToTopButton.className = "top-link hide";
-  }
-};
 
 
 
@@ -84,7 +72,6 @@ anime({
 	duration: 2000,	
 	loop: true
 });
-
 
 
 
@@ -298,6 +285,21 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 initPhotoSwipeFromDOM('.mygallery');
 
 
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
 
 window.addEventListener("scroll", scrollFunc);
 
